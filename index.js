c7e5a4c3e22754c05661185cf6746d5007f44182
Daichi Yoshida <d.y.0110.barcelona@gmail.com>
2018-05-02 22:22:04 +0900

    クエリーパラメーターの利用
    req.query.パラメーターで簡単に取り出せる

--- index.js
+++ index.js
@@ -15,22 +15,28 @@ app.use(express.static('public'));
 app.get(    '/' ,   (req,res)   =>  {   //function(req,res){....}と同じ
     
     var msg = "This is Express-app Top Page!<br>"    +   "これはトップページです。";
+    //パラメーターの設定
+    var url = '/other?name=taro&pass=yamada';
     //index.ejsのレンダリング  
     res.render('index.ejs',{
         title:'Index',
         content:msg,
-        link:{href:'/other',text:'*Otherページに移動'}
+        link:{href:url,text:'パラメーター送信'}
     });
 });
 
 //ルーティングを設定する(otherページ)
 app.get(    "/other"    ,   (req,res)   =>  {
-    var msg = "This is Other Page!<br>" + "これはotherページです。";
+    //パラメーターの取り出し
+    var name = req.query.name;
+    var pass = req.query.pass;
+    //メッセージの設定
+    var msg = "あなたは「" + name +"」<br>パスワードは「" + pass + "」です。";
     //otherページのレンダリング
     res.render('index.ejs',{
         title:'Other',
         content:msg,
-        link:{href:'/',text:'*トップへ戻る'}
+        link:{href:'/',text:'＊トップへ戻る'}
     });
 });
 
